c26c7d8b7d53b0520a0ae519f97690a580b0b55c
zerocnc <gdi_richard@yahoo.com>
2021-07-17 01:30:48 -0700

    Initial push and end of exercise.

--- CalculatorTutorial/Calculator.cpp
+++ CalculatorTutorial/Calculator.cpp
@@ -1,1 +1,17 @@
 #include "Calculator.h"
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+    switch (oper) {
+    case '+':
+        return x + y;
+    case '-':
+        return x - y;
+    case '*':
+        return x * y;
+    case '/':
+        return x / y;
+    default:
+        return 0.0;
+    }
+}
--- CalculatorTutorial/CalculatorTutorial.cpp
+++ CalculatorTutorial/CalculatorTutorial.cpp
@@ -2,22 +2,37 @@
 //
 
 #include <iostream>
+#include "Calculator.h"
 
 int main()
 {
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
     std::cout << "Calculator Console Application" << std::endl << std::endl;
     std::cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
         << std::endl;
+
+
+    Calculator c;
+
+    while (true)
+    {
+        std::cin >> x >> oper >> y;
+
+        if (oper == '/' && y == 0) {
+            std::cout << "Division by 0 exception" << std::endl;
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
+
+        std::cout << "Result is: " << result << std::endl;
+    }
+
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
